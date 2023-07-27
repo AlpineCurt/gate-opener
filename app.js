16f2059205c9ba8a47ecec5dc86b23379f240b9c
Curt <alpinecurt@gmail.com>
2023-07-26 18:24:26 -0700

    setup 4

--- app.js
+++ app.js
@@ -3,20 +3,26 @@
 const express = require("express");
 const Voice = require("twilio/lib/rest/Voice");
 const VoiceReponse = require("twilio").twiml.VoiceResponse;
-const { GATE_NUM } = require("./config.js");
+const { GATE_NUM, PAUSES, PRESSES } = require("./config.js");
 
 const app = express();
 
 app.post("/voice", (req, res) => {
     const response = new VoiceReponse();
+    let digitsText = ""
+    for (let i = 0; i < PAUSES; i++) {
+        digitsText += "w";
+    }
+    for (let i = 0; i < PRESSES; i ++) {
+        digitsText += GATE_NUM;
+    }
+    debugger;
+
     response.play({
-        digits: `www${GATE_NUM}${GATE_NUM}${GATE_NUM}`
+        digits: digitsText
     });
     res.type("text/xml");
     res.send(response.toString());
-    return res.json({
-        phone: "post request received"
-    });
 });
 
 app.get("/test", (req, res) => {
--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 const PORT = +process.env.PORT || 3001;
 const GATE_NUM = process.env.GATE_NUM || 9;
+const PAUSES = process.env.PAUSES || 5;
+const PRESSES = process.env.PRESSES || 3;
 
 module.exports = {
     PORT,
-    GATE_NUM
+    GATE_NUM,
+    PAUSES,
+    PRESSES
 }
